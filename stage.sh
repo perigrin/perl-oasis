4f76962056486eacfe44f70e0c986adc53936f42
Chris Prather <chris@prather.org>
2012-01-15 14:44:50 -0500

    sync in the opw2012 site

--- stage.sh
+++ stage.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -x
-rsync -avz ./actdocs/ act:/home/apache/htdocs/conferences-test/actdocs/opw2011/
-rsync -avz ./wwwdocs/ act:/home/apache/htdocs/conferences-test/wwwdocs/opw2011/
+rsync -avz ./actdocs/ act:/home/apache/htdocs/conferences-test/actdocs/opw2012/
+rsync -avz ./wwwdocs/ act:/home/apache/htdocs/conferences-test/wwwdocs/opw2012/
