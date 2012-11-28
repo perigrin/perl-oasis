15ce2d69bc9a71c95d4716cc0525a48571ac74d7
Chris Prather <chris@prather.org>
2012-11-28 17:35:48 -0500

    prepare for 2013

--- stage.sh
+++ stage.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -x
-rsync -avz ./actdocs/ act:/home/apache/htdocs/conferences-test/actdocs/opw2012/
-rsync -avz ./wwwdocs/ act:/home/apache/htdocs/conferences-test/wwwdocs/opw2012/
+rsync -avz ./actdocs/ act:/home/apache/htdocs/conferences-test/actdocs/opw2013/
+rsync -avz ./wwwdocs/ act:/home/apache/htdocs/conferences-test/wwwdocs/opw2013/
